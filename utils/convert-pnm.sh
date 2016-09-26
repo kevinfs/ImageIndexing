34b03979fd0cda2984bb83887244c6e5003ff549
kevinfs <kevinfs@me.com>
2016-09-26 15:44:57 +0200

    bash script to convert png files to p(g|p)m

---
+++ utils/convert-pnm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+FILES=~/IMG/*.jpg
+
+for f in $FILES
+do
+  echo "Processing file $f..."
+
+#  filename=$(basename "$f")
+  filename=$f
+
+  filename="${filename%.*}"
+  pgm=$filename
+  pgm+='.pgm'
+  ppm=$filename
+  ppm+='.ppm'
+
+  convert $f $pgm
+  echo 'pgm [OK]'
+
+  convert $f $ppm
+  echo "ppm [OK]"
+
+done
