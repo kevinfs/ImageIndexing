3b73f6bdb666d6e435afea52be01bcb3ac58cd43
SN1885A <lisefl.aubin@gmail.com>
2016-09-27 12:47:43 +0200

    add function to compute RGB rate

---
+++ utils/computeRateRGB.c
@@ -0,0 +1,24 @@
+#include "computeRateRGB.h"
+
+TauxRGB computeTauxRGB(rgb8 **imageRGB, long nrl,long nrh,long ncl,long nch){
+
+	int y, x, i, nbPixels;
+	TauxRGB tauxRGB;
+	double sumR = 0, sumG = 0, sumB = 0, moyR, moyG, moyB;
+
+	for(y=nrl;y<=nrh;y++){
+		for(x=ncl;x<=nch;x++){
+			sumR += imageRGB[y][x].r;
+			sumG += imageRGB[y][x].g;
+			sumB += imageRGB[y][x].b;		
+		}
+	}
+
+	nbPixel = x*y;
+	
+	moyR = sumR/nbPixels;
+	moyG = sumG/nbPixels;
+	moyB = sumB/nbPixels;
+	
+return tauxRGB;
+}
---
+++ utils/computeRateRGB.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "def.h"
+#include "nrio.h"
+#include "nrarith.h"
+#include "nralloc.h"
+
+typedef struct{
+	double tauxR;
+	double tauxG;
+	double tauxB;
+}TauxRGB;
