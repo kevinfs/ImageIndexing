0ff4450f0a6cd1299de43258419e3b1eb671e6be
Mehdi Abd <mahito78260@gmail.com>
2016-09-27 18:27:39 +0900

    [Add] histograms functions

---
+++ utils/histogram.c
@@ -0,0 +1,29 @@
+#include "histogram.h"
+
+int* histogram1Channel(byte **image, long nrl, long nrh, long ncl, long nch){
+	int hist[256] = { 0 };
+	int i = 0;
+	int x, y;
+	for (x = nrl; x <= nrh; x++){
+		for (y = ncl; y <= nch; y++){
+			hist[image[x][y]]++;
+		}
+	}
+	return hist;
+}
+
+int** histogram3Channel(rgb8 **imageRGB, long nrl, long nrh, long ncl, long nch){
+	int hist[3][256] = { { 0 } };
+	int x, y;
+	int r = 0;
+	int g = 1;
+	int b = 2;
+	for (x = nrl; x <= nrh; x++){
+		for (y = ncl; y <= nch; y++){
+			hist[r][imageRGB[x][y].r]++;
+			hist[g][imageRGB[x][y].g]++;
+			hist[b][imageRGB[x][y].b]++;
+		}
+	}
+	return hist;
+}
---
+++ utils/histogram.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "def.h"
+#include "nrio.h"
+#include "nrarith.h"
+#include "nralloc.h"
+
+int* histogram1Channel(byte **image, long nrl, long nrh, long ncl, long nch);
+int** histogram3Channel(rgb8 **imageRGB, long nrl, long nrh, long ncl, long nch);
