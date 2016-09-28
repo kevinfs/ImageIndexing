ccb0891a4d4ca195afe9fab8f5486663465ec747
kevinfs <kevinfs@me.com>
2016-09-28 15:59:14 +0200

    Basic descriptor

--- src/app/ImageDescriptor.java
+++ src/app/ImageDescriptor.java
@@ -7,5 +7,13 @@ package app;
 public class ImageDescriptor {
 	
 	private String fileName;
+	private String colorType;
+	private double gradientMean;
+	private double ratioTexture;
+	private double ratioR;
+	private double ratioG;
+	private double ratioB;
+	private Histogram histogram;
+
 
 }
