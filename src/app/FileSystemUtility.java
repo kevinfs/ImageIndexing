5d0f77d4ecbad2c7772184be3e4352f284eede9c
kevinfs <kevinfs@me.com>
2016-09-28 16:45:39 +0200

    FileSystemUtility

---
+++ src/app/FileSystemUtility.java
@@ -0,0 +1,19 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileSystemUtility {
+
+	// TODO get dynamic basename
+	static public String basename = "/Users/Jarvis/Documents/workspace old/ImageIndexing/";
+	static public String imageDir = basename + "utils/images/";
+	
+	List<String> getAllImagesNames() {
+		List<String> result = new ArrayList<String>();
+		
+		// TODO get all files in a directory
+		
+		return result;
+	}
+}
