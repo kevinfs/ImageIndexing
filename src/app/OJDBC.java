788574604d63ce0678e7bc01bc7d14d4cf213cf6
SN1885A <lisefl.aubin@gmail.com>
2016-09-30 02:01:13 +0200

    [add] simpleDIstNbPixelsShape function

--- src/app/OJDBC.java
+++ src/app/OJDBC.java
@@ -1,18 +1,10 @@
 package app;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+
 import java.sql.SQLException;
-import java.sql.Types;
 import java.util.ArrayList;
 
 import oracle.jdbc.OracleCallableStatement;
@@ -20,7 +12,7 @@ import oracle.jdbc.OracleTypes;
 import oracle.sql.ARRAY;
 import oracle.sql.ArrayDescriptor;
 import oracle.sql.Datum;
-import oracle.sql.STRUCT;
+
 
 public class OJDBC {
 
@@ -48,20 +40,16 @@ public class OJDBC {
 		}
 	}
 
-	public void addImageData(int txR, int txG, int txB, int nbPixels,
-			int color, int[] hist) {
+	public void addImageData(int txR, int txG, int txB, int nbPixels, int color, int[] hist) {
 		try {
-			ArrayDescriptor arrayDescriptor = ArrayDescriptor.createDescriptor(
-					"HISTOGRAMM", connection);
+			ArrayDescriptor arrayDescriptor = ArrayDescriptor.createDescriptor("HISTOGRAMM", connection);
 
 			ARRAY array = new ARRAY(arrayDescriptor, connection, hist);
 
-			String insertQuery = "UPDATE MULTIMEDIA SET hist = ?, TXR = " + txR
-					+ ", TXG = " + txG + ",TXB = " + txB + " , NBPIXELS = "
-					+ nbPixels + ", COLOR =" + color + "  WHERE nom = '1.jpg' ";
+			String insertQuery = "UPDATE MULTIMEDIA SET hist = ?, TXR = " + txR + ", TXG = " + txG + ",TXB = " + txB
+					+ " , NBPIXELS = " + nbPixels + ", COLOR =" + color + "  WHERE nom = '1.jpg' ";
 
-			PreparedStatement preparedStatement = connection
-					.prepareStatement(insertQuery);
+			PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
 			preparedStatement.setArray(1, array);
 
 			preparedStatement.executeUpdate();
@@ -72,99 +60,130 @@ public class OJDBC {
 			System.err.println(se.getMessage());
 		}
 	}
-	
+
 	public ArrayList<String> bhattacharyaHist(String name, double seuil) {
-		
+
 		ArrayList<String> al = new ArrayList<String>();
-		
+
 		try {
-			
+
 			String insertQuery = "begin batacharyaHistProc( ?, ?, ?); end; ";
-			OracleCallableStatement preparedStatement =  (OracleCallableStatement)connection.prepareCall(insertQuery);
-     		
+			OracleCallableStatement preparedStatement = (OracleCallableStatement) connection.prepareCall(insertQuery);
+
 			preparedStatement.registerOutParameter(1, OracleTypes.ARRAY, "BATRESULT");
-     		preparedStatement.setString(2, name);
-			preparedStatement.setDouble(3, seuil);	
+			preparedStatement.setString(2, name);
+			preparedStatement.setDouble(3, seuil);
 
 			preparedStatement.execute();
 			ARRAY array_to_pass = preparedStatement.getARRAY(1);
 			Datum[] elements = array_to_pass.getOracleArray();
-			
-			for (int i=0;i<elements.length;i++){
+
+			for (int i = 0; i < elements.length; i++) {
 				al.add(elements[i].stringValue());
 			}
-			
+
 			preparedStatement.close();
-			
+
 		} catch (SQLException se) {
 			System.err.println(se.getMessage());
 		}
-		
+
 		return al;
 	}
-	
+
 	public ArrayList<String> bhattacharyaTx(String name, double seuil) {
-		
+
 		ArrayList<String> al = new ArrayList<String>();
-		
+
 		try {
-			
-			String insertQuery = "begin batacharyaTxProc( ?, ?, ?); end; ";
-			OracleCallableStatement preparedStatement =  (OracleCallableStatement)connection.prepareCall(insertQuery);
-     		
+
+			String insertQuery = "begin batacharyaTxProc(?, ?, ?); end; ";
+			OracleCallableStatement preparedStatement = (OracleCallableStatement) connection.prepareCall(insertQuery);
+
 			preparedStatement.registerOutParameter(1, OracleTypes.ARRAY, "BATRESULT");
-     		preparedStatement.setString(2, name);
-			preparedStatement.setDouble(3, seuil);	
+			preparedStatement.setString(2, name);
+			preparedStatement.setDouble(3, seuil);
 
 			preparedStatement.execute();
 			ARRAY array_to_pass = preparedStatement.getARRAY(1);
 			Datum[] elements = array_to_pass.getOracleArray();
-			
-			for (int i=0;i<elements.length;i++){
+
+			for (int i = 0; i < elements.length; i++) {
 				al.add(elements[i].stringValue());
 			}
-			
+
 			preparedStatement.close();
-			
+
 		} catch (SQLException se) {
 			System.err.println(se.getMessage());
 		}
-		
+
 		return al;
 	}
-	
-	public ArrayList<String> signature(String name, double color, double texture, double shape, double locationn, double seuil) {
-			
-			ArrayList<String> al = new ArrayList<String>();
-			
-			try {
-				
-				String insertQuery = "begin SigLoop(?, ?, ?, ?, ?, ?, ?); end; ";
-				OracleCallableStatement preparedStatement =  (OracleCallableStatement)connection.prepareCall(insertQuery);
-	     		
-				preparedStatement.registerOutParameter(1, OracleTypes.ARRAY, "SIGRESULT");
-	     		preparedStatement.setString(2, name);
-				preparedStatement.setDouble(3, color);
-				preparedStatement.setDouble(4, texture);
-				preparedStatement.setDouble(5, shape);
-				preparedStatement.setDouble(6, locationn);
-				preparedStatement.setDouble(7, seuil);
 
-				preparedStatement.execute();
-				ARRAY array_to_pass = preparedStatement.getARRAY(1);
-				Datum[] elements = array_to_pass.getOracleArray();
-				
-				for (int i=0;i<elements.length;i++){
-					al.add(elements[i].stringValue());
-				}
-				
-				preparedStatement.close();
-				
-			} catch (SQLException se) {
-				System.err.println(se.getMessage());
+	public ArrayList<String> simpleDistNbPixelsShape(String name, double seuil) {
+
+		ArrayList<String> al = new ArrayList<String>();
+
+		try {
+
+			String insertQuery = "begin NBPIXELSLoop( ?, ?, ?); end; ";
+			OracleCallableStatement preparedStatement = (OracleCallableStatement) connection.prepareCall(insertQuery);
+
+			preparedStatement.registerOutParameter(1, OracleTypes.ARRAY, "SIGRESULT");
+			preparedStatement.setString(2, name);
+			preparedStatement.setDouble(3, seuil);
+
+			preparedStatement.execute();
+			ARRAY array_to_pass = preparedStatement.getARRAY(1);
+			Datum[] elements = array_to_pass.getOracleArray();
+
+			for (int i = 0; i < elements.length; i++) {
+				al.add(elements[i].stringValue());
 			}
-			
-			return al;
+
+			preparedStatement.close();
+
+		} catch (SQLException se) {
+			System.err.println(se.getMessage());
+		}
+
+		return al;
+	}
+
+	public ArrayList<String> signature(String name, double color, double texture, double shape, double locationn,
+			double seuil) {
+
+		ArrayList<String> al = new ArrayList<String>();
+
+		try {
+
+			String insertQuery = "begin SigLoop(?, ?, ?, ?, ?, ?, ?); end; ";
+			OracleCallableStatement preparedStatement = (OracleCallableStatement) connection.prepareCall(insertQuery);
+
+			preparedStatement.registerOutParameter(1, OracleTypes.ARRAY, "SIGRESULT");
+			preparedStatement.setString(2, name);
+			preparedStatement.setDouble(3, color);
+			preparedStatement.setDouble(4, texture);
+			preparedStatement.setDouble(5, shape);
+			preparedStatement.setDouble(6, locationn);
+			preparedStatement.setDouble(7, seuil);
+
+			preparedStatement.execute();
+			ARRAY array_to_pass = preparedStatement.getARRAY(1);
+			Datum[] elements = array_to_pass.getOracleArray();
+
+			for (int i = 0; i < elements.length; i++) {
+				al.add(elements[i].stringValue());
+			}
+
+			preparedStatement.close();
+
+		} catch (SQLException se) {
+			System.err.println(se.getMessage());
+		}
+
+		return al;
 	}
 
 }
